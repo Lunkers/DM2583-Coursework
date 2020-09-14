228743aa4dc3ba554d146a70c6ac00d960fbcf4b
Foppe <flundkvi@kth.se>
2020-09-14 13:32:27 +0200

    start of lab 1

---
+++ Lab 1/NaiveBayes.py
@@ -0,0 +1,13 @@
+#import pandas requirements
+import pandas as pd
+from pandas import DataFrame
+from sklearn.pipeline import Pipeline
+from sklearn.naive_bayes import MultinomialNB #use multinomial naive bayes
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+
+#read in data
+#test
+#training
+#evaluation?
+
